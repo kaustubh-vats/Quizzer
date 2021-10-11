4a96d38e9e1824e32db61492f84729b17f057710
kaustubh-vats <kaustubhvats08@gmail.com>
2021-10-11 17:06:37 +0530

    fixed nav bar isssue

---
+++ static/js/header.js
@@ -0,0 +1,18 @@
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');  
+    const nav = document.querySelectorAll('.nav-links li');
+    const bar = document.querySelector('.nav-links');
+    burger.addEventListener('click',()=>{
+        console.log("hello");
+        bar.classList.toggle('nav-active');
+        nav.forEach((link,index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 + 0.5}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
